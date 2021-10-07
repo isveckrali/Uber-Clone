cceeabf7bfcb0ef2904366a66181ea66b2dcd63b
Mehmet Can Seyhan <13541515.mehmetcan.seyhan@gmail.com>
2021-10-07 21:10:07 +0200

    Initial Commit

---
+++ Uber Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Uber Clone
+//
+//  Created by Mehmet Can Seyhan on 2021-10-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
